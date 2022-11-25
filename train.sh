7148fba45f7cae240e0b9ea407a2907595c02956
Uynixu <44133702+Uynixu@users.noreply.github.com>
2022-11-25 10:50:31 +0800

    Delete train.sh

--- train.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#BSUB -q gpu
-#BSUB -gpu "num=1:mode=exclusive_process"
-#BSUB -o %J.out
-#BSUB -e %J.err
-#BSUB -R "span[ptile=1]"
-
-python -u ./train.py -d YAGO --batch_size 1024 --max_epochs 20
